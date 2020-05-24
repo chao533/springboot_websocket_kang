4273f32606d9b00a38f9c866006cbc25ba7a0219
Administrator <Administrator@PC-201906261744>
2020-05-24 12:09:39 +0800

    edit

--- src/main/java/com/kang/WebSocketApplication.java
+++ src/main/java/com/kang/WebSocketApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebSocketApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello webSocket");
+		System.out.println("hello chao");
 		SpringApplication.run(WebSocketApplication.class, args);
 	}
 }
